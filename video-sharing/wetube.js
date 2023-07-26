e55dbe48dce6660eb3245eab99bfc64fa9bb3b31
glmck13 <glmck13@verizon.net>
2023-07-26 05:56:23 -0400

    Add files via upload

--- video-sharing/wetube.js
+++ video-sharing/wetube.js
@@ -1,21 +1,33 @@
 var wwwRoot = window.location.href.split("/")[0];
 
 var weTubeYellowPages = {
-    "Name-1" :		["9XXXXXXXXXX", "webRTC-ext", "webRTC-ext"],
-    "Name-2" :		["9XXXXXXXXXX", "webRTC-ext", "webRTC-ext"],
-    "Name-3" :		["9XXXXXXXXXX", "webRTC-ext", "webRTC-ext"],
-    "Name-3" :		["9XXXXXXXXXX", "webRTC-ext", "webRTC-ext"],
-    "..." :		["9XXXXXXXXXX", "webRTC-ext", "webRTC-ext"]
+    "Frannie" :		["",            "7160", "7160"],
+    "Dad" :		["95184613259", "7150", "7150"],
+    "Mom" :		["94109495047", "7140", "7140"],
+    "UncleDaniel" :	["98605978618", "7130", "7130"],
+    "KK" :		["94438388483", "7120", "7120"],
+    "DiDi" :		["94432554925", "7110", "7110"],
+    "G" :		["94437569795", "7100", "7100"]
 }
+var weTubeServer = "mckspot.net";
 
 var initDB = window.localStorage;
-initDB.setItem("wssServer", "");
+var weTubeExt = weTubeYellowPages[weTubeUser][1];
+initDB.setItem("wssServer", weTubeServer);
 initDB.setItem("WebSocketPort", "8089");
 initDB.setItem("ServerPath", "/ws");
 initDB.setItem("profileName", weTubeUser);
-initDB.setItem("SipDomain","");
-initDB.setItem("SipUsername", weTubeYellowPages[weTubeUser][1]);
-initDB.setItem("SipPassword", "");
+initDB.setItem("SipDomain",weTubeServer);
+initDB.setItem("SipUsername", weTubeExt);
+initDB.setItem("SipPassword", "789889");
+initDB.setItem("VoiceMailSubscribe", "0");
+
+initDB.setItem("ChatEngine", "XMPP");
+initDB.setItem("XmppServer", weTubeServer);
+initDB.setItem("XmppWebsocketPort", "7443");
+initDB.setItem("XmppWebsocketPath", "/ws");
+initDB.setItem("XmppDomain", weTubeServer);
+initDB.setItem("profileUser", weTubeExt);
 
 async function blobToBase64 (blob) {
   var reader = new FileReader();
@@ -40,18 +52,22 @@ function InitGlobalBuddies () {
 
     var dateNow = utcDateNow();
     var buddyObj = null;
-    var id = uID();
 
-    var id, key, val, pstn, sip, webRTC, buddyObj;
+    var id, jid, key, val, pstn, sip, webRTC, buddyObj;
 
     for(key in weTubeYellowPages) {
-        id = uID();
         val = weTubeYellowPages[key]; pstn = val[0]; webRTC = val[1]; sip = val[2];
-        savePic((key == weTubeUser) ? "profilePicture" : "img-" + id + "-extension", wwwRoot + "/avatars/" + key + ".webp");
-        if (key == weTubeUser) continue;
+        if (key == weTubeUser) {
+        	savePic("profilePicture", wwwRoot + "/avatars/" + key + ".webp");
+		continue;
+	}
+	else {
+	        id = uID(); jid =  sip + "@" + weTubeServer;
+		savePic("img-" + id + "-extension", wwwRoot + "/avatars/" + key + ".webp");
+	}
 
         json.DataCollection.push({
-            Type: "extension",
+            Type: "xmpp",
             LastActivity: dateNow,
             ExtensionNumber: sip ? sip : pstn,
             MobileNumber: pstn,
@@ -60,18 +76,18 @@ function InitGlobalBuddies () {
             uID: id,
             cID: null,
             gID: null,
-            jid: null,
+            jid: jid,
             DisplayName: key,
             Description: "",
             Email: "",
             MemberCount: 0,
             EnableDuringDnd: false,
-            Subscribe: false,
+            Subscribe: true,
             SubscribeUser: "",
             AutoDelete: false
         });
-        buddyObj = new Buddy("extension", id, key, webRTC, pstn, "", "", dateNow, "", "", null, false, false, null, false);
-        AddBuddy(buddyObj, false, false, false, true);
+        buddyObj = new Buddy("xmpp", id, key, webRTC, pstn, "", "", dateNow, "", "", jid, false, false, null, false);
+	AddBuddy(buddyObj, false, false, false, true);
     }
     json.TotalRows = json.DataCollection.length;
     localDB.setItem(me + "-Buddies", JSON.stringify(json));
