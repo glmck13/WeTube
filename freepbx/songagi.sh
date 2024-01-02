6b606759e84bbd067fe1f928321901a4702461b2
glmck13 <glmck13@verizon.net>
2024-01-02 14:09:17 -0500

    Delete freepbx/songagi.sh

--- freepbx/songagi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-digit=${1:-0} dir=${2:-0}
-song=$(ls /var/lib/asterisk/sounds/${dir}/${digit}[_-]* 2>/dev/null)
-song=${song##*/sounds/} song=${song%.*}
-
-[ "$song" ] && echo "SET VARIABLE SONG_FILE ${song}"
